ee7bbf1809fee220d6828c7cb3ea338186f1b61f
cdminigun <doege.chris@yahoo.com>
2017-01-20 15:39:54 -0600

    Added config files.

---
+++ Exercises/src/mynameis.go
@@ -0,0 +1,13 @@
+// A file to print out the user's name to standard output. 
+// Very similar to hello world.
+// Remember to include quotations around your import strings. 
+
+
+// Package declaration goes here
+
+//Import goes here
+
+//main and remainder of code goes here
+func main(){
+    //Your code here
+}
